d11cd4d0e81f6d6a6dfbd4784e896f3bb0aa09c3
readycade <122501283+readycade@users.noreply.github.com>
2024-06-07 16:52:36 -0400

    up

--- share/system/custom.sh
+++ share/system/custom.sh
@@ -477,6 +477,8 @@ install_binary "mount-zip" "https://github.com/readycade/readysync/raw/master/sh
         # Replace the following line with the actual command to start emulation station
         chvt 1; es start
 
+exit 0
+
 }
 
 # Function to switch to offline mode
