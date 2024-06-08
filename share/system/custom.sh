b3f1decc636418897fcebfa7b4524b4235113919
readycade <122501283+readycade@users.noreply.github.com>
2024-06-08 15:46:55 -0400

    up

--- share/system/custom.sh
+++ share/system/custom.sh
@@ -16,7 +16,10 @@
 ## This script is intended for educational and informational purposes only. The authors and ReadyCade, Inc. do not support or condone the illegal downloading or distribution of video games. Downloading video games
 ## without proper authorization is illegal and can result in severe penalties. Users are solely responsible for ensuring that their actions comply with applicable laws.
 
-## This script does not actually download or store any video games. It is solely for mounting an online source, and no content will remain on your ReadyCade upon reboot.
+## This script does not actually download or store any video games for 69 consoles/systems. It is solely for mounting an online source, and no content will remain on your ReadyCade upon reboot.
+
+## TOSEC Romsets ARE downloaded and extracted to your Readycade as long as they are ENABLED in the script.
+## DISABLED consoles/systems will have all their contents deleted next time the script runs (Next Reboot.)
 
 ## In no event shall the authors or ReadyCade, Inc. be liable for any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the script
 ## or the use or other dealings in the script. USE AT YOUR OWN RISK. YOU ASSUME ALL LIABILITY FOR ANY ACTIONS TAKEN BASED ON THIS SCRIPT.
