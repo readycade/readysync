a44b023cfe6fe88bc6697a17c89c35f5682283c9
Mike Cabral <mike.cabral@disroot.org>
2024-09-27 00:57:14 -0400

    update

--- share/system/custom.sh
+++ share/system/custom.sh
@@ -426,32 +426,8 @@ fi
 #rclone mount redump: /recalbox/share/rom/Redump --config=/recalbox/share/system/rclone2.conf --daemon --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --allow-non-empty &
 #rclone mount tosec: /recalbox/share/rom/TOSEC --config=/recalbox/share/system/rclone2.conf --daemon --no-checksum --no-modtime --attr-timeout 100h --dir-cache-time 100h --poll-interval 100h --allow-non-empty &
 
-# Attempt to download rclone4.conf
-if [ ! -f /recalbox/share/system/rclone4.conf ] || [ ! -s /recalbox/share/system/rclone4.conf ]; then
-    echo "rclone4.conf is missing or is 0KB, attempting to download..."
-    if wget --no-check-certificate -q --retry-connrefused --tries=3 --timeout=10 --waitretry=5 --read-timeout=10 --retry-on-http-error=500,502,503,504 https://raw.githubusercontent.com/readycade/readysync/refs/heads/master/share/userscripts/.config/readystream/rclone4.conf -O /recalbox/share/system/rclone4.conf; then
-        if [ -s /recalbox/share/system/rclone4.conf ]; then
-            echo "rclone4.conf downloaded successfully."
-        else
-            echo "rclone4.conf is still 0KB after download."
-        fi
-    else
-        echo "Failed to download rclone4.conf after 3 attempts."
-    fi
-else
-    echo "rclone4.conf exists and is valid, skipping download."
-fi
-
-    # Attempt to mount rclone4.conf
-    if rclone mount olddos: /recalbox/share/dos --config "/recalbox/share/system/rclone4.conf" --http-no-head --no-checksum --no-modtime --attr-timeout 365d --dir-cache-time 365d --poll-interval 365d --allow-non-empty --daemon --no-check-certificate; then
-        echo "Rclone mounted old-dos.ru successfully."
-    else
-        echo "Failed to mount olddos."
-    fi
-
 echo "Mounting romsets..."
 echo "(No-Intro, Redump, TOSEC)..."
-echo "(Old-Dos.ru)..."
 
 #wait
 
