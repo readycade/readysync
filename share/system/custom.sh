23b93322107ed1c91a79ae59b4425922bf252297
Mike Cabral <mike.cabral@disroot.org>
2024-09-26 21:16:04 -0400

    up

--- share/system/custom.sh
+++ share/system/custom.sh
@@ -428,8 +428,13 @@ fi
 
 # Attempt to download rclone4.conf
 if [ ! -f /recalbox/share/system/rclone4.conf ]; then
-    if wget -q --retry-connrefused --tries=3 https://raw.githubusercontent.com/readycade/readysync/refs/heads/master/share/userscripts/.config/readystream/rclone4.conf -O /recalbox/share/system/rclone4.conf; then
-        echo "rclone4.conf downloaded successfully."
+    if wget -q --retry-connrefused --tries=3 --timeout=10 --waitretry=5 --read-timeout=10 --retry-on-http-error=500,502,503,504 https://raw.githubusercontent.com/readycade/readysync/refs/heads/master/share/userscripts/.config/readystream/rclone4.conf -O /recalbox/share/system/rclone4.conf; then
+        if [ -s /recalbox/share/system/rclone4.conf ]; then
+            echo "rclone4.conf downloaded successfully."
+        else
+            echo "rclone4.conf downloaded but is empty, retrying..."
+            rm /recalbox/share/system/rclone4.conf
+        fi
     else
         echo "Failed to download rclone4.conf after 3 attempts."
     fi
