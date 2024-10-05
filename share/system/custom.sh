26d505218a66dbf05692c9f1343ad5cf5f382080
Mike Cabral <mike.cabral@disroot.org>
2024-10-04 23:23:02 -0400

    up

--- share/system/custom.sh
+++ share/system/custom.sh
@@ -562,8 +562,8 @@ for remote in "${!mounts[@]}"; do
     if rclone mount "$remote" "$temp_mount" --config "$conf_file" --http-no-head --no-checksum --no-modtime --attr-timeout 365d --dir-cache-time 365d --poll-interval 365d --allow-non-empty --daemon --no-check-certificate; then
         echo "Rclone mounted $remote successfully."
         
-        # Use mergerfs to combine the temp mount and the actual directory
-        mergerfs "$temp_mount:${mounts[$remote]}" "${mounts[$remote]}" -o defaults,allow_other
+        # Only merge if the temp mount was successful
+        mergerfs "$temp_mount" "${mounts[$remote]}" -o defaults,allow_other
     else
         echo "Failed to mount $remote..."
     fi
